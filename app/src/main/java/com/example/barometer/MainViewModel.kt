8d7904da233b7c6e2d6782164c59e0bee2890c00
yu-ko-ba <yu-ko-ba@outlook.com>
2022-06-18 12:32:42 +0900

    SensorEventListenerとしての機能を別クラスとして切り出した

--- app/src/main/java/com/example/barometer/MainViewModel.kt
+++ app/src/main/java/com/example/barometer/MainViewModel.kt
@@ -1,35 +1,25 @@
 package com.example.barometer
 
-import android.hardware.Sensor
-import android.hardware.SensorEvent
-import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
-class MainViewModel(private val sensorManager: SensorManager) : ViewModel(), SensorEventListener {
+class MainViewModel(sensorManager: SensorManager) : ViewModel() {
     val airPressureLiveData: MutableLiveData<String> by lazy {
         MutableLiveData<String>()
     }
 
-    private val airPressureSensor: Sensor? = sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
-
-    override fun onSensorChanged(event: SensorEvent) {
-        airPressureLiveData.value = event.values[0].toString()
-    }
-
-    override fun onAccuracyChanged(sensor: Sensor, accuracy: Int) {
-        // センサーの精度が変わったときの処理を記述する
-        // NOOP
+    private val airPressureSensorEventListener = AirPressureSensorEventListener(sensorManager) {
+        airPressureLiveData.value = it
     }
 
     fun registerAirPressureSensor() {
-        sensorManager.registerListener(this, airPressureSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        airPressureSensorEventListener.registerAirPressureSensor()
     }
 
     fun unregisterAirPressureSensor() {
-        sensorManager.unregisterListener(this)
+        airPressureSensorEventListener.unregisterAirPressureSensor()
     }
 
     class Factory(private val sensorManager: SensorManager) : ViewModelProvider.Factory {
