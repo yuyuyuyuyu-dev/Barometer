ee073f9d5f83a54a492ce3170c8f9cc81f5b756c
yu <59269129+yu-ko-ba@users.noreply.github.com>
2025-06-07 15:14:42 +0900

    feat(data): implement BarometricPressureRepositoryImpl

---
+++ app/src/androidTest/java/io/github/yukoba/barometer/data/repositories/BarometricPressureRepositoryImplTest.kt
@@ -0,0 +1,41 @@
+package io.github.yukoba.barometer.data.repositories
+
+import android.content.Context
+import android.hardware.SensorManager
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BarometricPressureRepositoryImplTest {
+    private lateinit var repository: BarometricPressureRepositoryImpl
+
+    private lateinit var sensorManager: SensorManager
+
+    @Before
+    fun setUp() {
+        val context = InstrumentationRegistry.getInstrumentation().targetContext
+
+        sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+
+        repository = BarometricPressureRepositoryImpl(sensorManager = sensorManager)
+    }
+
+    @Test
+    // this test success if device has barometric sensor
+    fun barometricPressure_emits_at_least_one_value() = runTest {
+        val firstValue = repository.barometricPressure.first()
+
+        // The value range is derived from the local atmospheric pressure at the highest and lowest elevations on Earth,
+        // factoring in fluctuations from weather phenomena.
+        // The fluctuation value is based on the difference between the record highest/lowest sea-level pressures
+        // and the standard atmospheric pressure.
+        assert(firstValue in 157.0..1156.8) {
+            "Expected pressure to be within the valid terrestrial range (157.0..1156.8), but it was $firstValue."
+        }
+    }
+}
--- app/src/main/java/io/github/yukoba/barometer/data/repositories/BarometricPressureRepositoryImpl.kt
+++ app/src/main/java/io/github/yukoba/barometer/data/repositories/BarometricPressureRepositoryImpl.kt
@@ -1,8 +1,47 @@
 package io.github.yukoba.barometer.data.repositories
 
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import io.github.yukoba.barometer.data.models.BarometricPressureRepository
+import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.flow.emptyFlow
 
-class BarometricPressureRepositoryImpl : BarometricPressureRepository {
-    override val barometricPressure: Flow<Float> = TODO()
+class BarometricPressureRepositoryImpl(
+    private val sensorManager: SensorManager,
+) : BarometricPressureRepository {
+    private val barometricPressureSensor: Sensor? by lazy {
+        sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE)
+    }
+
+    override val barometricPressure: Flow<Float> = if (barometricPressureSensor == null) {
+        emptyFlow()
+    } else {
+        callbackFlow {
+            val listener = object : SensorEventListener {
+                override fun onSensorChanged(event: SensorEvent?) {
+                    if (event?.sensor?.type == Sensor.TYPE_PRESSURE) {
+                        trySend(event.values.first())
+                    }
+                }
+
+                override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+                    // NOOP
+                }
+            }
+
+            sensorManager.registerListener(
+                listener,                          // listener: android.hardware.SensorEventListener
+                barometricPressureSensor,          // sensor: android.hardware.Sensor
+                SensorManager.SENSOR_DELAY_NORMAL, // samplingPeriodUs: int
+            )
+
+            awaitClose {
+                sensorManager.unregisterListener(listener)
+            }
+        }
+    }
 }
