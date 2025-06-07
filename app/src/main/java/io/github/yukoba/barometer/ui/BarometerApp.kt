fd130ad55d13977b929b164de24de04f929ab931
yu <59269129+yu-ko-ba@users.noreply.github.com>
2025-06-08 08:37:19 +0900

    feat(ui): add top app bar

--- app/src/main/java/io/github/yukoba/barometer/ui/BarometerApp.kt
+++ app/src/main/java/io/github/yukoba/barometer/ui/BarometerApp.kt
@@ -4,11 +4,15 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import com.slack.circuit.backstack.rememberSaveableBackStack
 import com.slack.circuit.foundation.CircuitCompositionLocals
 import com.slack.circuit.foundation.NavigableCircuitContent
 import com.slack.circuit.foundation.rememberCircuitNavigator
+import dev.yuyuyuyuyu.simpletopappbar.SimpleTopAppBar
+import io.github.yukoba.barometer.R
 import io.github.yukoba.barometer.ui.barometer.BarometerScreen
+import io.github.yukoba.barometer.ui.openSourceLicensesList.OpenSourceLicenseListScreen
 import io.github.yukoba.barometer.ui.theme.BarometerTheme
 import org.koin.compose.KoinContext
 import org.koin.compose.koinInject
@@ -20,7 +24,25 @@ fun BarometerApp() {
 
     KoinContext {
         BarometerTheme {
-            Scaffold { innerPadding ->
+            Scaffold(
+                topBar = {
+                    val currentScreen = backStack.topRecord?.screen
+
+                    SimpleTopAppBar(
+                        title = stringResource(
+                            when (currentScreen) {
+                                is OpenSourceLicenseListScreen -> R.string.open_source_licenses
+                                else -> R.string.app_name
+                            }
+                        ),
+                        navigateBackIsPossible = backStack.size > 1,
+                        onNavigateBackButtonClick = { navigator.pop() },
+                        onOpenSourceLicensesButtonClick = {
+                            navigator.goTo(OpenSourceLicenseListScreen)
+                        },
+                    )
+                }
+            ) { innerPadding ->
                 CircuitCompositionLocals(koinInject()) {
                     NavigableCircuitContent(
                         navigator = navigator,
