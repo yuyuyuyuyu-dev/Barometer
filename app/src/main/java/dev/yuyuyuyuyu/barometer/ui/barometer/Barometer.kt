4d9c325f88c9b2620bca21cd6c15f28868be3ba0
yu <email@yuyuyuyuyu.dev>
2025-06-15 15:37:00 +0900

    feat(ui): adjustment line height to improve readability

--- app/src/main/java/dev/yuyuyuyuyu/barometer/ui/barometer/Barometer.kt
+++ app/src/main/java/dev/yuyuyuyuyu/barometer/ui/barometer/Barometer.kt
@@ -7,6 +7,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
 import dev.yuyuyuyuyu.barometer.R
 import dev.yuyuyuyuyu.barometer.ui.barometer.models.PressureState
@@ -23,5 +24,36 @@ fun Barometer(state: BarometerScreen.State, modifier: Modifier = Modifier) = Box
             is PressureState.Loading -> stringResource(R.string.loading)
         },
         fontSize = 34.sp,
+        lineHeight = 51.sp,
+    )
+}
+
+@Preview
+@Composable
+fun SuccessBarometerPreview() {
+    Barometer(
+        state = BarometerScreen.State(
+            pressure = PressureState.Success("731.149 hPa"),
+        ),
+    )
+}
+
+@Preview
+@Composable
+fun FailureBarometerPreview() {
+    Barometer(
+        state = BarometerScreen.State(
+            pressure = PressureState.Failure,
+        ),
+    )
+}
+
+@Preview
+@Composable
+fun LoadingBarometerPreview() {
+    Barometer(
+        state = BarometerScreen.State(
+            pressure = PressureState.Loading,
+        ),
     )
 }
