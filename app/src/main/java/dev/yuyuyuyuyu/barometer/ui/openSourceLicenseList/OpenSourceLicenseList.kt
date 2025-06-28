51e7e04da8cddecd41b348b6efca85ab2952428e
yu <email@yuyuyuyuyu.dev>
2025-06-28 13:38:44 +0900

    feat(ui.openSourceLicenseList): eliminate library duplication

--- app/src/main/java/dev/yuyuyuyuyu/barometer/ui/openSourceLicenseList/OpenSourceLicenseList.kt
+++ app/src/main/java/dev/yuyuyuyuyu/barometer/ui/openSourceLicenseList/OpenSourceLicenseList.kt
@@ -1,11 +1,22 @@
 package dev.yuyuyuyuyu.barometer.ui.openSourceLicenseList
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import com.mikepenz.aboutlibraries.ui.compose.android.rememberLibraries
 import com.mikepenz.aboutlibraries.ui.compose.m3.LibrariesContainer
+import dev.yuyuyuyuyu.barometer.R
+import kotlinx.collections.immutable.toImmutableList
 
 @Composable
-fun OpenSourceLicenseList(modifier: Modifier = Modifier) = LibrariesContainer(
-    modifier = modifier,
-    showDescription = true,
-)
+fun OpenSourceLicenseList(modifier: Modifier = Modifier) {
+    val libraries by rememberLibraries(R.raw.aboutlibraries)
+
+    LibrariesContainer(
+        libraries = libraries?.libraries?.distinctBy { it.name }?.let {
+            libraries?.copy(libraries = it.toImmutableList())
+        },
+        modifier = modifier,
+        showDescription = true,
+    )
+}
