acac45f261be04dbdab67afa4d780ca072ee371d
yu <email@yuyuyuyuyu.dev>
2025-07-13 10:23:36 +0900

    feat(ui): add padding to Barometer screen, using dimensionResource

--- app/src/main/java/dev/yuyuyuyuyu/barometer/ui/barometer/Barometer.kt
+++ app/src/main/java/dev/yuyuyuyuyu/barometer/ui/barometer/Barometer.kt
@@ -2,20 +2,23 @@ package dev.yuyuyuyuyu.barometer.ui.barometer
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
 import dev.yuyuyuyuyu.barometer.R
 import dev.yuyuyuyuyu.barometer.ui.barometer.models.BarometerState
 
 @Composable
 fun Barometer(state: BarometerScreen.State, modifier: Modifier = Modifier) = Box(
-    modifier = modifier.fillMaxSize(),
+    modifier = modifier
+        .fillMaxSize()
+        .padding(dimensionResource(R.dimen.screen_padding)),
     contentAlignment = Alignment.Center,
 ) {
     Text(
